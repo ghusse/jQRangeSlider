d059873ee6d8bc47ff37ab90c570ad44a84b7f54
Guillaume Gautreau <guillaume+github@ghusse.com>
2012-03-28 22:55:59 +0200

    Label positionning

--- jQRangeSliderLabel.js
+++ jQRangeSliderLabel.js
@@ -13,11 +13,70 @@
 
 	$.widget("ui.rangeSliderLabel", {
 		options: {
-			handle: null
+			handle: null,
+			formatter: false
 		},
 
+		cache: null,
+
 		_create: function(){
+			var left = this.options.handle.rangeSliderHandle("option", "isLeft");
+
+			this.element
+				.addClass("ui-rangeSlider-label")
+				.toggleClass("ui-rangeSlider-leftLabel", left)
+				.toggleClass("ui-rangeSlider-rightLabel", !left)
+				.css("position", "absolute");
+
+			this.options.handle.bind("moving", $.proxy(this._onMoving, this));
 			
+			(function(that){
+				setTimeout(function(){
+					that._initPosition()
+				}, 100);
+			})(this);
+		},
+
+		_initPosition: function(){
+			this._cache();
+			this._position();
+		},
+
+		_cache: function(){
+			this.cache = {};
+			this.cache.handle = {
+				width: this.options.handle.width()
+			};
+
+			this.cache.width = {
+				outer: this.element.outerWidth()
+			}
+		},
+
+		_cacheIfNot: function(){
+			if (this.cache === null){
+				this._cache();
+			}
+		},
+
+		/*
+		 * Event binding
+		 */
+		_onMoving: function(event, ui){
+			this._position(ui.offset.left);
+		},
+
+		/*
+		 * Positioning
+		 */
+		_position: function(left){
+			this._cacheIfNot();
+			
+			var offset = this.element.offset();
+
+			offset.left = left + this.cache.handle.width / 2 - this.cache.width.outer / 2;
+
+			this.element.offset(offset);
 		}
 	});
 
