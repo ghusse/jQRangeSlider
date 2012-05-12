079cbfc61f1977cbd2d274b2d8dec254abbbf39a
Guillaume Gautreau <guillaume+github@ghusse.com>
2012-05-12 23:50:02 +0200

    Round option removed on edit slider

--- demo/editSliderDemo.js
+++ demo/editSliderDemo.js
@@ -10,7 +10,6 @@
 			$.ui.sliderDemo.prototype._createOptions.apply(this);
 
 			this._createInputTypeOption();
-			this._createRoundOption();
 		},
 
 		_createInputTypeOption: function(){
@@ -18,19 +17,6 @@
 			var select = $("<select name='type'></select>")
 				.append("<option value='number'>number</option>")
 				.append("<option value='text' selected='selected'>text</option>");
-
-			this._createDD(select);
-
-			select.change($.proxy(this._easyOptionChange, this));
-		},
-
-		_createRoundOption: function(){
-			this._createDT("Round");
-
-			var select = $("<select name='round'></select>")
-				.append("<option value='false'>false</option>")
-				.append("<option value='1' selected='selected'>1</option>")
-				.append("<option value='10'>10</option>");
 
 			this._createDD(select);
 
